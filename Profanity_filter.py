9e094bba39221afd1c7d4a1d4c2538332d33cee2
MischaJampen <mischa.jampen@gmail.com>
2024-01-11 19:30:37 +0100

    change of file Profanity_filter

--- Profanity_filter.py
+++ Profanity_filter.py
@@ -3,34 +3,27 @@ __author__ = "Mischa Jampen"
 class ProfanityFilter:
 
     def __init__(self, keywords, template):
-        self.keywords = reversed(sorted([kw.lower() for kw in keywords]))
+        self.keywords = [kw.lower() for kw in keywords]
         self.template = template
 
     def filter(self, msg):
-        msg_lower = msg.lower()
-        for word in self.keywords:
-            while word in msg_lower:
-                idx = msg_lower.find(word)
-                msg = self.replace(msg, idx, word)
-                msg_lower = self.replace(msg_lower, idx, word)
-        return msg
-
-    def replace(self, s, idx, word):
-        clean_word = self.escape(word)
-        return s[:idx] + clean_word + s[idx+len(clean_word):]
-
-    def escape(self, word):
-        res = ""
-        for idx, _ in enumerate(word):
-            t_idx = idx % len(self.template)
-            res += self.template[t_idx]
-        return res
-
+        filtered_msg = []
+        for word in msg.lower().split():
+            filtered_word = word
+            for kw in self.keywords:
+                if word == kw:
+                    num_repetition = len(kw) // len(self.template)
+                    remainder = len(kw) % len(self.template)
+                    filtered_word = self.template * num_repetition + self.template[:remainder]
+                    break
+            filtered_msg.append(filtered_word)
+                
+        return " ".join(filtered_msg)
 # You can play around with your implementation in the body of the following 'if'.
 # The contained statements will be ignored while evaluating your solution.
 if __name__ == '__main__':
     f = ProfanityFilter(["duck", "shot", "batch", "mastard"], "?#$")
-    offensive_msg = "abc defghi mastard jklmno"
+    offensive_msg = "abc defghi mastard duck jklmno"
     clean_msg = f.filter(offensive_msg)
     print(clean_msg)  # abc defghi ?#$?#$? jklmno
     
